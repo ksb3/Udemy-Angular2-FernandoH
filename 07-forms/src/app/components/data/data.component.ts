aa808cca70dc04cdf6f472575cbd7ddae778da45
Kepa Sarasola <ksb3naiz@gmail.com>
2019-02-18 22:38:04 +0100

    data example

--- 07-forms/src/app/components/data/data.component.ts
+++ 07-forms/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
 
 @Component({
@@ -11,24 +11,60 @@ export class DataComponent {
 
   forma:FormGroup;
 
+  usuario:Object = {
+    nombrecompleto: {
+      nombre: 'kepa',
+      apellido: 'herrera'
+    },
+    correo: 'ksb@ksb3.com',
+    pasatiempos: ['Correr', 'Dormir', 'Comer']
+  }
+
 
   constructor() {
 
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('', Validators.required),
-      'apellido': new FormControl('', Validators.required),
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        'apellido': new FormControl('', Validators.required),
+      }),
       'correo': new FormControl('', [
                                       Validators.required,
                                       Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
-                                    ])
+                                    ]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
+      ])
 
     });
 
+    //this.forma.setValue(this.usuario);
+
+   }
+
+   agregarPasatiempo() {
+     (<FormArray>this.forma.controls['pasatiempos']).push(
+       new FormControl('Dormir', Validators.required)
+     )
    }
 
    guardarCambios(){
      console.log(this.forma.value);
      console.log(this.forma);
+
+     this.forma.reset({
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+     });
    }
 
 }
